69b2a371e0a72ec926ef3fa8bd1512827b7f2e21
Pride2002 <malenpride02@gmail.com>
2026-05-23 20:44:26 +0300

    added lost schema users

--- backend/models/users.js
+++ backend/models/users.js
@@ -5,6 +5,17 @@ const userSchema = mongoose.Schema({
   email: { type: String, required: true, unique: true },
   password: { type: String, required: true },
   confirmPassword: { type: String, required: true },
-});
+  cohort: { type: Number, required: true },
+  avatar: { type: String },
+  bio: { type: String },
+  skills: [{ type: String }],
+  asks: [{ type: String }],
+  role: {
+    type: String,
+    enum: ['member', 'admin', 'mentor'],
+    default: 'member'
+  },
+ 
+}, { timestamps: true });
 
 module.exports = mongoose.model("User", userSchema);
